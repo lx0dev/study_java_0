e6a1fec7901f9ef28f7dca84449ab039046319da
lx0dev <44697051+lx0dev@users.noreply.github.com>
2018-11-05 15:55:19 +0800

    test

--- src/study_java_0.java
+++ src/study_java_0.java
@@ -1,4 +1,11 @@
 public class study_java_0 {
-    //hello
-    //hello2
+    public static void main(String[] args) {
+        int a[][] = new int [3][4];
+        for (int i = 0; i<a.length; i++) {
+            for (int j = 0; j<a[i].length; j++) {
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
